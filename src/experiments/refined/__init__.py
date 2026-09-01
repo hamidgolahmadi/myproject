4dbc15687694e352f58da248e918ccd2786232a9
hamidgolahmadi <hamid.golahmadi@gmail.com>
2026-09-01 17:50:56 +0100

    Export refined paired treatment preparation

--- src/experiments/refined/__init__.py
+++ src/experiments/refined/__init__.py
@@ -9,9 +9,19 @@ from .paired import (
     ReplicationSeeds,
     prepare_paired_replication,
 )
+from .treatments import (
+    NonNetworkInitialConditions,
+    PreparedTopologyTreatment,
+    TopologySpecification,
+    prepare_paired_treatments,
+)
 
 __all__ = [
+    "NonNetworkInitialConditions",
     "PairedReplicationPlan",
+    "PreparedTopologyTreatment",
     "ReplicationSeeds",
+    "TopologySpecification",
     "prepare_paired_replication",
+    "prepare_paired_treatments",
 ]
